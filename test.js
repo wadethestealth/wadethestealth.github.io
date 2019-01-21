7065716ff89155b2d7ffc9e12c637c29cfadc23d
Thomas Denton <wadeuk3@gmail.com>
2019-01-21 15:40:46 -0600

    Delete test.js
    Test for receiving XHR was a success.

--- test.js
+++
@@ -1,9 +0,0 @@
-var url = 'https://api.github.com';
-var response;
-var request = new XMLHttpRequest();
-request.open('GET', url);
-request.onload = function() {
-  response = request.response;
-  console.log(response);
-};
-request.send();
